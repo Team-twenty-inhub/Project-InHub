45fb9f1ab93e0cad8772bd07755e77015a23530e
KyeongRyeolKR <rlarudfuf802@naver.com>
2023-05-22 17:09:26 +0900

    feat: Member Entity 필드 추가

--- src/main/java/com/twenty/inhub/boundedContext/member/entity/Member.java
+++ src/main/java/com/twenty/inhub/boundedContext/member/entity/Member.java
@@ -3,30 +3,46 @@ package com.twenty.inhub.boundedContext.member;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 import static lombok.AccessLevel.PROTECTED;
 
-@Entity @Getter
+@Entity
+@Getter
 @Builder(toBuilder = true)
 @AllArgsConstructor
 @NoArgsConstructor(access = PROTECTED)
 public class Member {
 
-    @Id @GeneratedValue(strategy = IDENTITY)
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
     private Long id;
 
     private String username;
     private String password;
+    private String nickname;
+    private String email;
+    private String profileImg;
 
+    private String token;
 
+    private LocalDateTime createDate;
+    private LocalDateTime modifyDate;
 
+//    private List<Question> questions;
+//    private List<Answer> answers;
+//    private List<Comment> comments;
+//    private List<Underline> underlines;
 
     //-- create authorize --//
     public List<? extends GrantedAuthority> getGrantedAuthorities() {
