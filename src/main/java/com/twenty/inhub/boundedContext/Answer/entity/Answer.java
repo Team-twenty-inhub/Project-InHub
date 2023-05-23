2b5e7878f82536d1d25c3b50133f170ef880f79e
YunJiW <dtroy371@gmail.com>
2023-05-23 16:08:39 +0900

    feat :answerService ì 추가

--- src/main/java/com/twenty/inhub/boundedContext/Answer/entity/Answer.java
+++ src/main/java/com/twenty/inhub/boundedContext/Answer/entity/Answer.java
@@ -4,10 +4,7 @@ import com.twenty.inhub.boundedContext.category.Category;
 import com.twenty.inhub.boundedContext.member.entity.Member;
 import com.twenty.inhub.boundedContext.question.entity.Question;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -35,9 +32,8 @@ public class Answer {
     @LastModifiedDate
     private LocalDateTime modifyDate;
 
-    private Integer questionAnswer;
-
     @Column(columnDefinition = "TEXT")
+    @Setter
     private String content;
 
     @ManyToMany
--- src/main/java/com/twenty/inhub/boundedContext/Answer/service/AnswerService.java
+++ src/main/java/com/twenty/inhub/boundedContext/Answer/service/AnswerService.java
@@ -7,9 +7,13 @@ import com.twenty.inhub.boundedContext.Answer.repository.AnswerRepository;
 import com.twenty.inhub.boundedContext.question.entity.Question;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class AnswerService {
 
     private final AnswerRepository answerRepository;
@@ -17,13 +21,51 @@ public class AnswerService {
     private final AnswerQueryRepository answerQueryRepository;
 
 
-    private RsData<Answer> create(Question question,String content){
-        Answer answer = Answer.builder().build();
+    //답변 생성
+    public RsData<Answer> create(String content){
+        Answer answer = Answer.builder()
+                .content(content)
+                .build();
 
 
-        answerRepository.save(answer);
+        this.answerRepository.save(answer);
 
-        return RsData.of("S-1","답변 등록 완료");
+        return RsData.of("S-1","답변 등록 완료",answer);
+    }
+
+    //답변 찾기
+    @Transactional(readOnly = true)
+    public Answer findAnswer(Long id){
+        Answer answer = this.answerRepository.findById(id).orElse(null);
+        return answer;
+    }
+
+    //정답 체크
+    @Transactional(readOnly = true)
+    public RsData<Answer> checkAnswer(Long id, String content){
+        Answer answer = findAnswer(id);
+
+        if(answer == null){
+            return RsData.failOf(answer);
+        }
+
+        if(!answer.getContent().equals(content)){
+            return RsData.of("F-34","오답",answer);
+        }
+
+        return RsData.of("S-55","정답",answer);
+    }
+
+    public Answer updateAnswer(Answer answer, String content){
+        answer.setContent(content);
+
+        return answer;
+    }
+
+
+    //답 삭제
+    public void deleteAnswer(Answer answer){
+        this.answerRepository.delete(answer);
     }
 
 
