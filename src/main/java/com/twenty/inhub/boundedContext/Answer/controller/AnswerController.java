cbbcd42c3b7a10a9f497886e1935f84ca6f73480
YunJiW <dtroy371@gmail.com>
2023-05-26 14:07:01 +0900

    feat :AnswerController CreateCheckAnswer 추가

--- src/main/java/com/twenty/inhub/boundedContext/Answer/controller/AnswerController.java
+++ src/main/java/com/twenty/inhub/boundedContext/Answer/controller/AnswerController.java
@@ -1,12 +1,22 @@
 package com.twenty.inhub.boundedContext.Answer.controller;
 
+import com.twenty.inhub.base.request.Rq;
+import com.twenty.inhub.base.request.RsData;
+import com.twenty.inhub.boundedContext.Answer.entity.Answer;
 import com.twenty.inhub.boundedContext.Answer.service.AnswerService;
+import com.twenty.inhub.boundedContext.member.entity.Member;
+import com.twenty.inhub.boundedContext.question.entity.Question;
+import com.twenty.inhub.boundedContext.question.service.QuestionService;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -14,20 +24,60 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/answer")
 public class AnswerController {
 
+    private final AnswerService answerService;
+
+    private final QuestionService questionService;
+
+    private final Rq rq;
 
     /**
-     *  AnswerForm
-     *  content
+     * AnswerForm
+     * keyword
+     */
+    //출제자가 적는 정답
+    @AllArgsConstructor
+    @Getter
+    public static class AnswerCheckForm {
+
+        @NotBlank
+        String word1;
+
+        String word2;
+
+        String word3;
+    }
+
+    /**
+     * AnswerForm
+     * content
      */
     //정답 적기용 Form
     @AllArgsConstructor
     @Getter
-    public static class AnswerForm{
+    public static class AnswerForm {
 
         @NotBlank
         private final String content;
     }
-    private AnswerService answerService;
 
+    //문제 출제자가 정답을 넣어 둘때
+    @PostMapping("/check/create/{id}")
+    @PreAuthorize("isAuthenticated()")
+    public String CreateCheckAnswer(AnswerCheckForm answerCheckForm, Long id, Model model) {
+
+        Member member = rq.getMember();
+
+        RsData<Question> question = this.questionService.findById(id);
+
+        if (question.isFail()) {
+            return rq.historyBack(question.getMsg());
+        }
+
+        RsData<Answer> answer = answerService.createAnswer(question.getData(), member, answerCheckForm.getWord1(), answerCheckForm.getWord2(), answerCheckForm.getWord3());
+        return rq.redirectWithMsg("/", answer.getMsg());
+    }
+
+
+    public
 }
 
