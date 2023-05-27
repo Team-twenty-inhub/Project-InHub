7f3149fca7c02f1fae9c65f298d0f8c9e776f147
I'm back <115536240+choideakook@users.noreply.github.com>
2023-05-27 12:05:35 +0900

    랜덤문제 기능 구현 / 이전문제, 다음문제 이동 기능은 아직 구현 안함 (#32)
    * fix: member import 삭제
    
    * feat: Question query dsl 적용
    
    * question, category 객체 생성 service 구현
    
    * test: category 생성 로직 검증 완료
    
    * test: 주관식 문제 생성 검증완료
    
    * teat: question 객관식 문제 생성 검증 완료
    
    * rebase 완료
    
    * design: html layout 구현
    
    * feat: category 생성 완료 / HTML 까지 완료
    
    * feat: category list 페이지 구현 완료
    
    * design: 네비바 레이아웃 변경
    
    * design: 네비바 레이아웃 변경 완료
    
    * feat: BaseEntity 생성
    
    * feat: category, question, tag, choice, underline 에 BaseEntity 상속
    
    * remove: 태그, 선택지 관련 코드 삭제 / 연관관계 매핑 형식으로 변경할 예정
    
    * feat: choice, tag 생성, 수정 entity 완료
    
    * feat: question service 생성 로직 구현, 테스트 완료
    
    * feat: question 생성 구현 / 문제 타입 아직 반영 안됬음
    
    * feat: category init data 추가
    
    * design: 카테고리 리스트 어드민 에게만 카테고리 생성 버튼 활성화
    
    * feat: 카테고리별 문제 목록 구현 완료
    
    * feat: 카테고리별 문제 목록 구현 완료
    
    * feat: 객관식 주관식 타입적용 완료 / init data 추가
    
    * feat: 문제 상세페이지 생성
    
    * fix: member 이넘타입 검증 안되는 문제 해결
    
    * design: 난이도 ui 업데이트
    
    * fix: 난이도 제대로 안나오는 문제 해결
    
    * feat: 문제 상세페이지 생성
    
    * design: 문제 상세페이지에 댓글, 밑줄긋기 버튼 추가
    
    * design: 문제 상페 뒤로가기 ui 업데이트
    
    * design: 문제 상세페이지 생성
    
    * chore: 오타수정
    
    * feat: 문제풀기 설정 폼 생성
    
    * design: 카테고리 체크박스 생성
    
    * design: 카테고리 체크박스 매핑 성공
    
    * feat: 난이도 제외 문제 풀기 설정 맵핑 성공
    
    * feat: 문제 생성폼의 모든 필드 매핑 성공
    
    * feat: 문제 풀기 폼 검증 로직 구현 완료
    
    * refactor: 문제 풀기 설정 html 코드  리팩토링
    
    * feat: 조건에 맞는 랜덤 문제 조회 로직 구현
    
    * fix: init data 가 test 에서도 작동되는 문제 확인
    
    * test: 조건에 맞는 랜덤 문제 조회 테스트 완료
    
    * design: 랜덤문제 풀기 생성
    
    * design: 랜덤 문제 ui 업데이트
    
    * fix: 문제풀기 설정에서 빈 카테고리 노출되는 문제 해결
    
    * design: 랜덤 문제 ui 업데이트
    
    * feat: 생성된 랜덤문제 세션에 저장 성공 / 지연로딩 문제 발생
    
    * feat: 지연로딩 문제를 해결하기 위해 랜덤 문제 생성시 id 필드만 조회 / test 성공
    
    * feat: 최근 실행한 랜덤문제 세션에 저장하면서 풀기 성공
    
    * design: 문제 풀기 ui update

--- src/main/java/com/twenty/inhub/boundedContext/question/controller/controller/QuestionFindController.java
+++ src/main/java/com/twenty/inhub/boundedContext/question/controller/controller/QuestionFindController.java
@@ -5,15 +5,20 @@ import com.twenty.inhub.base.request.RsData;
 import com.twenty.inhub.boundedContext.category.Category;
 import com.twenty.inhub.boundedContext.category.CategoryService;
 import com.twenty.inhub.boundedContext.member.entity.MemberRole;
+import com.twenty.inhub.boundedContext.question.controller.form.CreateFunctionForm;
 import com.twenty.inhub.boundedContext.question.entity.Question;
+import com.twenty.inhub.boundedContext.question.entity.QuestionType;
 import com.twenty.inhub.boundedContext.question.service.QuestionService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 import static com.twenty.inhub.boundedContext.question.entity.QuestionType.MCQ;
 
@@ -64,5 +69,52 @@ public class QuestionFindController {
         model.addAttribute("question", question);
         log.info("문제 상세페이지 응답 완료 category id = {}", id);
         return "usr/question/top/detail";
+    }
+
+    //-- 문제 풀기 설정폼 --//
+    @GetMapping("/function")
+    @PreAuthorize("isAuthenticated()")
+    public String function(CreateFunctionForm form, Model model) {
+        log.info("문제 풀기 설정폼 요청 확인");
+
+        List<Category> categories = categoryService.findAll();
+        List<QuestionType> types = questionService.findQuestionType();
+        List<Integer> difficulties = questionService.findDifficultyList();
+
+        model.addAttribute("difficulties", difficulties);
+        model.addAttribute("categories", categories);
+        model.addAttribute("types", types);
+
+        log.info("문제 설정폼 응답 완료");
+        return "usr/question/top/function";
+    }
+
+    //-- 랜덤 문제 리스트 생성 --//
+    @GetMapping("/playlist")
+    @PreAuthorize("isAuthenticated()")
+    public String playlist(CreateFunctionForm form) {
+        log.info("문제 리스트 생성 요청 확인 question count = {}", form.getCount());
+
+        List<Long> playlist = questionService.getPlaylist(form);
+        rq.getSession().setAttribute("playlist", playlist);
+
+        log.info("랜덤 문제 응답 완료 question count = {}", playlist.size());
+        return "usr/question/top/playlist";
+    }
+
+    //-- 랜덤 문제 실행 --//
+    @GetMapping("/play")
+    @PreAuthorize("isAuthenticated()")
+    public String play(Model model) {
+        List<Long> playlist = (List<Long>) rq.getSession().getAttribute("playlist");
+        log.info("문제 리스트 실행 요청 확인 playlist size = {}", playlist.size());
+
+        List<Question> questions = questionService.findByIdList(playlist);
+
+        model.addAttribute("questions", questions);
+        model.addAttribute("mcq", MCQ);
+
+        log.info("문제 리스트 실행");
+        return "usr/question/top/play";
     }
 }
--- src/main/java/com/twenty/inhub/boundedContext/question/repository/QuestionQueryRepository.java
+++ src/main/java/com/twenty/inhub/boundedContext/question/repository/QuestionQueryRepository.java
@@ -7,23 +7,65 @@ import com.twenty.inhub.boundedContext.question.entity.QuestionType;
 import jakarta.persistence.EntityManager;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 @Repository
 public class QuestionQueryRepository {
 
     private final JPAQueryFactory query;
+    private QQuestion question = QQuestion.question;
 
     public QuestionQueryRepository(EntityManager em) {
         this.query = new JPAQueryFactory(em);
     }
 
-    public List<Question> findByChoice() {
-        QQuestion question = QQuestion.question;
 
+    //-- play list 생성 (지연로딩으로 인해서 세션에 저장할 수 없음)
+    public List<Question> play(List<Long> id, List<QuestionType> type, List<Integer> difficulties, Integer count) {
+
+
+        List<Question> questions = query.selectFrom(question)
+                .where(question.category.id.in(id)
+                        .and(question.type.in(type))
+                        .and(question.difficulty.in(difficulties)))
+                .fetch();
+
+        // 랜덤한 순서로 정렬하기 위해 랜덤 값을 생성하여 정렬에 활용
+        long seed = System.nanoTime();
+        Collections.shuffle(questions, new Random(seed));
+
+        // count 만큼 잘라내기
+        return questions.subList(0, Math.min(count, questions.size()));
+    }
+
+    //-- play list id 로만 조회 --//
+    public List<Long> playlist(List<Long> id, List<QuestionType> type, List<Integer> difficulties, Integer count) {
+
+        List<Long> questionIds = query.select(question.id)
+                .from(question)
+                .where(question.category.id.in(id)
+                        .and(question.type.in(type))
+                        .and(question.difficulty.in(difficulties)))
+                .fetch();
+
+        // 랜덤한 순서로 정렬하기 위해 랜덤 값을 생성하여 정렬에 활용
+        long seed = System.nanoTime();
+        Collections.shuffle(questionIds, new Random(seed));
+
+        // count 만큼 잘라내기
+        return questionIds.subList(0, Math.min(count, questionIds.size()));
+    }
+
+    //-- find by id --//
+    public List<Question> findById(List<Long> id) {
         return query
                 .selectFrom(question)
-                .where(question.type.eq(QuestionType.MCQ))
+                .where(question.id.in(id))
                 .fetch();
     }
 }
+
+
+
