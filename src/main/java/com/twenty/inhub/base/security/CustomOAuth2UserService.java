a13532ef645fbf04ddc55f80a7036768824c8a47
KyeongRyeolKR <119229381+KyeongRyeolKR@users.noreply.github.com>
2023-05-31 08:37:35 +0900

    feat: 각 소셜 API 마다 회원 프로필 사진 가져오기 기능 (#38)
    * feat: Member Entity 필드 추가
    
    * feat: Member Entity 필드 추가
    
    * fix: QMember 수정
    
    * feat: MemberService.create() 일반 회원가입 구현 및 테스트
    
    * refactor: Member 필드 1대N 관계 추가
    
    * feat: Member.updateRole() 및 AppConfig 추가
    
    * feat: 소셜 로그인 구현 준비
    
    * feat: 소셜로그인 구현을 위한 yml 작업
    
    * feat: 로그인 기능 추가
    
    * design: 로그인 페이지 디자인
    
    * feat: 메인 페이지 추가
    
    * design: 메인 타이틀 수정
    
    * feat: 밑줄 친 문제 목록 보기 기능 추가
    
    * refactor: 밑줄 친 문제 목록 로직 수정
    
    * chore: 밑줄 친 목록 보기 매핑 URL 변경
    
    * feat: 내가 만든 문제 목록 보기 추가
    
    * feat: InitData 생성, admin 계정 추가 및 권한 설정
    
    * design: 관리자 로그인을 위한 디자인 작업
    
    * design: 내정보 페이지 디자인
    
    * 충돌 수정
    
    * feat: 프로필 이미지 기능 추가
    
    * design: 프로필 이미지 디자인
    
    * design: 프로필 이미지 파일 업로드 시 미리보기 기능 추가
    
    * refactor: 프로필 수정 폼 추가 및 로직 변경
    
    * refactor: 프로필 변경에서 이미지를 업로드 안했을 때 기존 이미지 사용
    
    * refactor: 닉네임 중복 체크
    
    * design: 밑줄 친 문제 목록 보기 디자인
    
    * refactor: 닉네임 수정 시, 변경 없으면 그대로 저장가능하게
    
    * refactor: 테스트용 데이터 추가
    
    * feat: 밑줄 친 문제 목록 리스팅 기능 추가
    
    * feat: 카카오 첫 로그인 시, 해당 회원의 프로필 이미지 가져오는 기능
    
    * feat: 깃허브 첫 로그인 시, 해당 회원의 프로필 이미지 가져오는 기능
    
    * feat: 구글 첫 로그인 시, 해당 회원의 프로필 이미지 가져오는 기능

--- src/main/java/com/twenty/inhub/base/security/CustomOAuth2UserService.java
+++ src/main/java/com/twenty/inhub/base/security/CustomOAuth2UserService.java
@@ -5,6 +5,7 @@ import com.twenty.inhub.boundedContext.member.service.MemberService;
 import com.twenty.inhub.boundedContext.member.entity.Member;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
@@ -13,6 +14,7 @@ import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Collection;
 import java.util.Map;
@@ -38,9 +40,46 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
             default -> oAuth2User.getName();
         };
 
+        String profileImg = "";
+
+        if(providerTypeCode.equals("GITHUB")) {
+            // 깃허브 API 호출을 위한 사용자 정보 가져오기
+            String accessToken = userRequest.getAccessToken().getTokenValue();
+            String apiUrl = "https://api.github.com/user";
+
+            RestTemplate restTemplate = new RestTemplate();
+            restTemplate.getInterceptors().add((request, body, execution) -> {
+                request.getHeaders().setBearerAuth(accessToken);
+                return execution.execute(request, body);
+            });
+
+            ResponseEntity<Map> response = restTemplate.getForEntity(apiUrl, Map.class);
+            Map<String, String> responseData = response.getBody();
+
+            profileImg = responseData.get("avatar_url");
+            System.out.println(profileImg);
+        }
+
+        if(providerTypeCode.equals("KAKAO")) {
+            Map<String, Object> map = oAuth2User.getAttributes();
+            System.out.println(map);
+            Map<String, String> properties = (Map<String, String>) map.get("properties");
+            System.out.println(properties);
+            profileImg = properties.get("profile_image");
+            System.out.println(profileImg);
+        }
+
+        if (providerTypeCode.equals("GOOGLE")) {
+            Map<String, Object> map = oAuth2User.getAttributes();
+            System.out.println(map);
+            String pictureUrl = (String) map.get("picture");
+            System.out.println(pictureUrl);
+            profileImg = pictureUrl;
+        }
+
         String username = providerTypeCode + "__%s".formatted(oauthId);
 
-        Member member = memberService.whenSocialLogin(providerTypeCode, username).getData();
+        Member member = memberService.whenSocialLogin(providerTypeCode, username, profileImg).getData();
 
         return new CustomOAuth2User(member.getUsername(), member.getPassword(), member.getGrantedAuthorities());
     }
