59ddf03b5a5370045ef703d45b565e9f09536526
KyeongRyeolKR <rlarudfuf802@naver.com>
2023-06-03 20:21:14 +0900

    feat: 티어 시스템 도입 준비

--- src/main/java/com/twenty/inhub/boundedContext/member/service/MemberService.java
+++ src/main/java/com/twenty/inhub/boundedContext/member/service/MemberService.java
@@ -196,4 +196,16 @@ public class MemberService {
     public Optional<Member> findByUsername(String username) {
         return memberRepository.findByUsername(username);
     }
+
+    // 임시 : 포인트를 올려주는 메소드
+    @Transactional
+    public void increasePoint(Member member) {
+        member.setPoint(member.getPoint()+100);
+    }
+
+    // 임시 : 포인트를 내려주는 메소드
+    @Transactional
+    public void decreasePoint(Member member) {
+        member.setPoint(member.getPoint()-100);
+    }
 }
