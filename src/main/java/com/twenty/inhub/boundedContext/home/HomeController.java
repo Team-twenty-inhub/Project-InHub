8300da585e92fddd6a0f33454fe0e5cc21b34d6d
I'm back <115536240+choideakook@users.noreply.github.com>
2023-06-07 13:40:53 +0900

    fix: 문제 풀고 결과 리스트 페이지로 연결 (#67)
    * design: 메인 페이지 ui 업데이트
    
    * design: 메인 페이지 이미지 업데이트
    
    * design: 객관식 상세페이지 정답 0번에 체크되게 임시로 변경
    
    * feat: 랜덤문제 정답 페이지 만드는중
    
    * fix: 문제 풀고 결과 리스트 페이지로 연결

--- src/main/java/com/twenty/inhub/boundedContext/home/HomeController.java
+++ src/main/java/com/twenty/inhub/boundedContext/home/HomeController.java
@@ -1,13 +1,16 @@
 package com.twenty.inhub.boundedContext.home;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Slf4j
 @Controller
 public class HomeController {
 
     @GetMapping("/")
     public String showMain() {
+        log.info("홈페이지 접속 요청 확인");
         return "/usr/index";
     }
 }
