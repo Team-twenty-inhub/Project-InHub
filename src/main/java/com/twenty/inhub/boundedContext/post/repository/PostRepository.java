d879470e4786897c358457e9cb7228444fe50b25
chkim429 <ckdgjs000@naver.com>
2023-06-14 22:29:22 +0900

    feat: 검색기능 구현

--- src/main/java/com/twenty/inhub/boundedContext/post/repository/PostRepository.java
+++ src/main/java/com/twenty/inhub/boundedContext/post/repository/PostRepository.java
@@ -1,8 +1,11 @@
 package com.twenty.inhub.boundedContext.post.repository;
 
+import com.twenty.inhub.boundedContext.post.dto.PostDto;
 import com.twenty.inhub.boundedContext.post.entity.Post;
+import com.twenty.inhub.boundedContext.question.entity.Question;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -13,5 +16,6 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     Page<Post> findAll(Pageable pageable);
     Page<Post> findByBoard(String board, Pageable pageable);
     List<Post> findByMemberId(Long memberId);
+    Page<Post> findAll(Specification<Post> spec, Pageable pageable);
 
 }
