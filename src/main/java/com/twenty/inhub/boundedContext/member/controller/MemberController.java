9929a9b11b93582d3f04c8bd06769dbc4892acfe
KyeongRyeolKR <rlarudfuf802@naver.com>
2023-06-21 13:46:45 +0900

    refactor: 회원가입 @Valid 사용

--- src/main/java/com/twenty/inhub/boundedContext/member/controller/MemberController.java
+++ src/main/java/com/twenty/inhub/boundedContext/member/controller/MemberController.java
@@ -13,31 +13,28 @@ import com.twenty.inhub.boundedContext.member.controller.form.MemberUpdateForm;
 import com.twenty.inhub.boundedContext.member.entity.Member;
 import com.twenty.inhub.boundedContext.member.service.MemberService;
 import com.twenty.inhub.boundedContext.member.service.PointService;
-import com.twenty.inhub.boundedContext.post.dto.PostDto;
 import com.twenty.inhub.boundedContext.post.entity.Post;
 import com.twenty.inhub.boundedContext.post.service.PostService;
 import com.twenty.inhub.boundedContext.question.entity.Question;
 import com.twenty.inhub.boundedContext.question.service.QuestionService;
 import com.twenty.inhub.boundedContext.underline.Underline;
 import com.twenty.inhub.boundedContext.underline.UnderlineService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Controller
@@ -63,13 +60,17 @@ public class MemberController {
 
     @PreAuthorize("isAnonymous()")
     @GetMapping("/join")
-    public String joinForm(MemberJoinForm form) {
+    public String joinForm() {
         return "usr/member/join";
     }
 
     @PreAuthorize("isAnonymous()")
     @PostMapping("/join")
-    public String join(MemberJoinForm form) {
+    public String join(@Valid MemberJoinForm form, Errors errors) {
+        if(errors.hasErrors()) {
+            return rq.historyBack("올바른 입력 형식이 아닙니다.");
+        }
+
         RsData<Member> rsData = memberService.create(form);
 
         log.info("회원가입 결과 = {}", rsData.getMsg());
--- src/main/java/com/twenty/inhub/boundedContext/member/controller/form/MemberJoinForm.java
+++ src/main/java/com/twenty/inhub/boundedContext/member/controller/form/MemberJoinForm.java
@@ -1,14 +1,24 @@
 package com.twenty.inhub.boundedContext.member.controller.form;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class MemberJoinForm {
-
+    @NotBlank
+    @Size(min = 4, message = "아이디를 4자 이상 입력해주세요.")
     private String username;
+    @NotBlank
+    @Size(min = 4, message = "비밀번호를 4자 이상 입력해주세요.")
     private String password;
+    @NotBlank
+    @Email
     private String email;
+    @NotBlank
+    @Size(min = 4, message = "닉네임을 4자 이상 입력해주세요.")
     private String nickname;
 }
