994324d283aa81fb63c2fd4c1fdeb79b8c027964
chkim429 <107919444+chkim429@users.noreply.github.com>
2023-06-22 13:30:20 +0900

    feat: 게시글에 마크다운 적용 (#134)
    * feat: 마크다운 에디터 구현
    
    * feat: view에서도 마크다운 적용

---
+++ src/main/java/com/twenty/inhub/boundedContext/markdown/MarkdownComponent.java
@@ -0,0 +1,16 @@
+package com.twenty.inhub.boundedContext.markdown;
+
+import org.commonmark.node.Node;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MarkdownComponent {
+    public String markdown(String markdown) {
+        Parser parser = Parser.builder().build();
+        Node document = parser.parse(markdown);
+        HtmlRenderer renderer = HtmlRenderer.builder().build();
+        return renderer.render(document);
+    }
+}
