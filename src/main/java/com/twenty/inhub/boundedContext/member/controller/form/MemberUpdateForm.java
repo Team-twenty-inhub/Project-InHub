f0dadc25955ef0a43bf46e148e94826574168d66
KyeongRyeolKR <rlarudfuf802@naver.com>
2023-06-23 10:17:02 +0900

    fix: prod 브랜치 나누기

--- src/main/java/com/twenty/inhub/boundedContext/member/controller/form/MemberUpdateForm.java
+++ src/main/java/com/twenty/inhub/boundedContext/member/controller/form/MemberUpdateForm.java
@@ -1,11 +1,17 @@
 package com.twenty.inhub.boundedContext.member.controller.form;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 @Data
 @AllArgsConstructor
 public class MemberUpdateForm {
 
+    @NotBlank
+    @Length(min = 2, max = 8, message = "닉네임을 2 ~ 8자 사이로 입력해주세요.")
+    @Pattern(regexp = "^(?=.*[a-z0-9ㄱ-힣])[a-z0-9가-힣ㄱ-ㅎㅏ-ㅣ]{2,8}$", message = "닉네임은 영문, 숫자, 한글로 구성된 2 ~ 8자리입니다.")
     private String nickname;
 }
