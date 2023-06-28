f011b5c735cd362f97ce2ee788f5bc3d06500a0b
choideakook <shdrnrhd113@naver.com>
2023-06-28 14:07:25 +0900

    feat: logback xml 추가

--- src/main/java/com/twenty/inhub/ut/jasypt/JasyptConfig.java
+++ src/main/java/com/twenty/inhub/ut/jasypt/JasyptConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableEncryptableProperties
-public class JasyptConfigDES {
+public class JasyptConfig {
 
     // 암호 키를 저장하는 필드
     @Value("${jasypt.encryptor.password}")
