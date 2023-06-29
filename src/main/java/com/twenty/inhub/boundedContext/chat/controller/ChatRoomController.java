096b6098af464939918a2e249013a929c06606c3
KyeongRyeolKR <119229381+KyeongRyeolKR@users.noreply.github.com>
2023-06-30 08:31:43 +0900

    feat: 채팅방 비활성화 및 삭제 기능 (#151)
    * fix: 기존 회원가입 ajax 오류 수정(csrf)
    
    * chore: 채팅 initData 추가
    
    * chore: 소소한 구조 변경
    
    * feat: 관리자 문의 채팅방 삭제 기능
    
    * fix: 연관관계 수정으로 채팅방 삭제 로직 수정
    
    * refactor: 채팅방 삭제 권한 수정 및 비활성화 기능 추가

--- src/main/java/com/twenty/inhub/boundedContext/chat/controller/ChatRoomController.java
+++ src/main/java/com/twenty/inhub/boundedContext/chat/controller/ChatRoomController.java
@@ -1,26 +1,33 @@
 package com.twenty.inhub.boundedContext.chat.controller;
 
 import com.twenty.inhub.base.request.Rq;
+import com.twenty.inhub.base.request.RsData;
 import com.twenty.inhub.boundedContext.chat.entity.ChatRoom;
 import com.twenty.inhub.boundedContext.chat.service.ChatRoomService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
+@Slf4j
 @RequiredArgsConstructor
+@RequestMapping("/rooms")
 public class ChatRoomController {
 
     private final ChatRoomService chatRoomService;
     private final Rq rq;
 
-    @GetMapping("/rooms")
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping
     public String showRooms(Model model) {
 
         List<ChatRoom> chatRooms = chatRoomService.findByUsername(rq.getMember().getUsername());
@@ -33,7 +40,8 @@ public class ChatRoomController {
         return "usr/chat/rooms";
     }
 
-    @GetMapping("/rooms/{roomId}")
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/{roomId}")
     public String showRoom(@PathVariable Long roomId, Model model) {
 
         ChatRoom chatRoom = chatRoomService.getByIdAndUserId(roomId, rq.getMember().getId());
@@ -44,15 +52,44 @@ public class ChatRoomController {
         return "usr/chat/room";
     }
 
-    @GetMapping("/rooms/new")
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/new")
     public String showNewRoom() {
         return "usr/chat/newRoom";
     }
 
-    @PostMapping("/rooms")
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping
     public String newRoom(String roomName) {
         ChatRoom chatRoom = chatRoomService.createAndSave(roomName, rq.getMember().getId());
 
         return rq.redirectWithMsg("/rooms/%d".formatted(chatRoom.getId()), "새로운 방 생성 성공");
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/delete/{roomId}")
+    public String deleteRoom(@PathVariable Long roomId) {
+        RsData<ChatRoom> rsData = chatRoomService.deleteRoom(rq.getMember(), roomId);
+        log.info("{}번 채팅방 삭제 결과 = {}", roomId, rsData.getResultCode());
+
+        if(rsData.isFail()) {
+            return rq.historyBack(rsData.getMsg());
+        }
+
+        log.info("삭제한 채팅방({}) = {}", rq.getMember().getUsername(), rsData.getData().getName());
+
+        return rq.redirectWithMsg("/rooms", rsData.getMsg());
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/disabled/{roomId}")
+    public String disabledRoom(@PathVariable Long roomId, Model model) {
+        RsData<ChatRoom> rsData = chatRoomService.finished(roomId);
+
+        model.addAttribute("chatRoom", rsData.getData());
+
+        log.info("해당 채팅방 disabled 상태 = {}", rsData.getData().isDisabled());
+
+        return rq.redirectWithMsg("/rooms/"+roomId, "채팅방이 비활성화 되었습니다.");
     }
 }
