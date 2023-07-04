c8b630441cd48a93f027f1aaf070879e6f9e4a08
choideakook <shdrnrhd113@naver.com>
2023-07-04 14:43:17 +0900

    feat: google ads 코드 스니펫 추가

--- src/main/java/com/twenty/inhub/base/firebase/FCMController.java
+++ src/main/java/com/twenty/inhub/base/firebase/FCMController.java
@@ -1,9 +1,14 @@
 package com.twenty.inhub.base.firebase;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 public class FCMController {
@@ -14,5 +19,15 @@ public class FCMController {
     public String test() {
         init.init();
         return "usr/firebase/firebase";
+    }
+
+    @ResponseBody
+    @PostMapping("/fcm/token")
+    public String addToken(
+            @RequestBody String token
+    ) {
+        log.info("token 저장 요청 확인 token = {}", token);
+
+        return "good";
     }
 }
