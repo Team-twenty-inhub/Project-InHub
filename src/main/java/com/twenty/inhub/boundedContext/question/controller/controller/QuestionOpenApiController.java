e90d9a7965d287b1b2cc7ae74c0239a8999345da
choideakook <shdrnrhd113@naver.com>
2023-07-09 15:10:15 +0900

    feat: question id api 구현

--- src/main/java/com/twenty/inhub/boundedContext/question/controller/controller/QuestionOpenApiController.java
+++ src/main/java/com/twenty/inhub/boundedContext/question/controller/controller/QuestionOpenApiController.java
@@ -1,10 +1,16 @@
 package com.twenty.inhub.boundedContext.question.controller.controller;
 
+import com.twenty.inhub.base.jwt.JwtProvider;
+import com.twenty.inhub.base.request.Rq;
+import com.twenty.inhub.base.request.RsData;
+import com.twenty.inhub.boundedContext.question.controller.dto.QuestionResDto;
+import com.twenty.inhub.boundedContext.question.entity.Question;
+import com.twenty.inhub.boundedContext.question.service.QuestionService;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -13,5 +19,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class QuestionOpenApiController {
 
+    private final QuestionService questionService;
+    private final Rq rq;
+    private final JwtProvider jwtProvider;
 
+    @GetMapping("/{id}")
+    public RsData findById(
+            @PathVariable Long id,
+            HttpServletRequest request
+    ) {
+        log.info("question id = {}", id);
+        String accessToken = request.getHeader("Bearer");
+        if (!jwtProvider.verify(accessToken))
+            throw new IllegalArgumentException("유효하지 않은 token");
+
+
+        RsData<Question> questionRs = questionService.findById(id);
+
+        if (questionRs.isFail()) {
+            throw new IllegalArgumentException("존재하지 않는 id");
+        }
+
+        log.info("question 응답 완료");
+        return RsData.of(new QuestionResDto(questionRs.getData()));
+    }
 }
