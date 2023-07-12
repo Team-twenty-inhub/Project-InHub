7cc541f68fed1d4a3ad8f0549a359a438db11e68
chkim429 <ckdgjs000@naver.com>
2023-07-12 11:27:06 +0900

    feat: 크롤링 데이터 페이징 구현

--- src/main/java/com/twenty/inhub/boundedContext/post/controller/MainServerController.java
+++ src/main/java/com/twenty/inhub/boundedContext/post/controller/MainServerController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
@@ -18,14 +19,24 @@ import java.util.List;
 @RequestMapping("/crawling")
 @RequiredArgsConstructor
 public class MainServerController {
+    private static final int PAGE_SIZE = 18;
+
     @GetMapping("/job-infos")
-    public String createPostFromCrawling(Model model) {
+    public String createPostFromCrawling(Model model, @RequestParam(value = "page", defaultValue = "0") int page) {
         RestTemplate restTemplate = new RestTemplate();
         CrawledJobDto[] list = restTemplate.getForObject("http://localhost:8081/crawling/job-infos", CrawledJobDto[].class);
 
         List<CrawledJobDto> crawledJobs = processCrawledJobs(list);
+        List<List<CrawledJobDto>> paginatedJobs = paginateJobs(crawledJobs);
 
-        model.addAttribute("Dto", crawledJobs);
+        // 현재 페이지 인덱스를 모델에 추가
+        model.addAttribute("currentPage", page);
+        // 전체 페이지 목록을 모델에 추가
+        model.addAttribute("pages", paginatedJobs);
+
+        // 현재 페이지에 해당하는 카드 목록을 추출
+        List<CrawledJobDto> currentPageCards = paginatedJobs.get(page);
+        model.addAttribute("cards", currentPageCards);
 
         return "usr/post/job";
     }
@@ -47,5 +58,21 @@ public class MainServerController {
             processedJobs.add(processedJob);
         }
         return processedJobs;
+    }
+
+    private List<List<CrawledJobDto>> paginateJobs(List<CrawledJobDto> crawledJobs) {
+        List<List<CrawledJobDto>> paginatedJobs = new ArrayList<>();
+        int totalJobs = crawledJobs.size();
+        int totalPages = (int) Math.ceil((double) totalJobs / PAGE_SIZE);
+
+        for (int i = 0; i < totalPages; i++) {
+            int fromIndex = i * PAGE_SIZE;
+            int toIndex = Math.min(fromIndex + PAGE_SIZE, totalJobs);
+
+            List<CrawledJobDto> page = crawledJobs.subList(fromIndex, toIndex);
+            paginatedJobs.add(page);
+        }
+
+        return paginatedJobs;
     }
 }
