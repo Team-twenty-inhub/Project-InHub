7f7e05ff4f9abfaefeee22c0eda7304047a61e0b
chkim429 <107919444+chkim429@users.noreply.github.com>
2023-07-13 17:03:02 +0900

    refactor: 전반적인 코드 수정 (#200)
    * feat: 마크다운 에디터 구현
    
    * feat: view에서도 마크다운 적용
    
    * feat: 파일 첨부 기능
    
    * feat: 첨부파일 다운로드 기능
    
    * chore: 다중 첨부기능 미구현
    
    * feat: 크롤링 서버ì에서 데이터를 가져와 메인 서버에 반환
    
    * chore: 충돌 해결
    
    * feat: 크롤링 데이터 페이징 구현
    
    * feat: 크롤링 데이터 검색 기능
    
    * chore: index에 채용 공고 창으로 이동되는 링크 생성
    
    * refactor: 서비스 클래스로 로직 분리
    
    * chore: ãyml 수정
    
    * fix: pull 해서 classê 중복 문제 해결
    
    * refactor: 전반적인 코드 수정

--- src/main/java/com/twenty/inhub/boundedContext/community/repository/CommunityRepository.java
+++
@@ -1,8 +0,0 @@
-package com.twenty.inhub.boundedContext.community.repository;
-
-;
-import com.twenty.inhub.boundedContext.community.entity.Community;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommunityRepository extends JpaRepository<Community, Long> {
-}
